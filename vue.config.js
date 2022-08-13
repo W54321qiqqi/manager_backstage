7bee0e42e564c18630ac5d40e0de8400ebeb4fef
qiyuhang <qiyuhang@gmail.com>
2022-08-13 11:01:14 +0800

    websocket_config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer:{
-    
-  }
-})
+    // 开发环境配置
+    devServer: {
+      host: "0.0.0.0",
+      // 运行时自动打开浏览器
+      open:true,
+      port: 9999,
+      // 解决热重载使用公网
+      client: {
+        webSocketURL: "ws://0.0.0.0:9999/ws",
+      },
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+});
