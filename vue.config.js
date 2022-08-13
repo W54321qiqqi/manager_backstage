26f58085f6acc2e2776ed300c888ea88056d5fbc
qiyuhang <qiyuhang@gmail.com>
2022-08-13 11:09:15 +0800

    jsconfig-exclude_change

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-    // 开发环境配置
-    devServer: {
-      host: "0.0.0.0",
-      // 运行时自动打开浏览器
-      open:true,
-      port: 9999,
-      // 解决热重载使用公网
-      client: {
-        webSocketURL: "ws://0.0.0.0:9999/ws",
-      },
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
+  // 开发环境配置
+  devServer: {
+    host: "0.0.0.0",
+    // 运行时自动打开浏览器
+    open: true,
+    port: 9999,
+    // 解决热重载使用公网
+    client: {
+      webSocketURL: "ws://0.0.0.0:9999/ws",
     },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  // 关闭eslint校验工具
+  lintOnSave: false,
 });
